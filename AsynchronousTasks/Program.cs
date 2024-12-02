c9d44d230845e4911bfe484f02dcd486e09638a9
thecodingrunner <finn.moffett@gmail.com>
2024-12-02 10:28:09 +0000

    Add project files.

---
+++ AsynchronousTasks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace AsynchronousTasks
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            //Thread.Sleep(2000);
+            //Console.WriteLine("Hello, World!");
+
+            await PrintHelloWorld();
+
+            static async Task PrintHelloWorld()
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                Task hello = Task.Run(async () => {
+                    await Task.Delay(3000);
+                    Console.WriteLine("Hello...");
+                });
+
+                Task world = Task.Run(async () => {
+                    await Task.Delay(3000);
+                    Console.WriteLine("...World!");
+                });
+
+                var combinedTask = Task.WhenAll([hello, world]);
+                await combinedTask;
+
+                stopwatch.Stop();
+                Console.WriteLine($"This code executed in {stopwatch.ElapsedMilliseconds}ms");
+            }
+        }
+    }
+}
